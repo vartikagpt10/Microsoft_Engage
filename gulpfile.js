0093094fe80942d57274f274b2eaf77ffb815560
jaihonikhil <jaihonikhil@gmail.com>
2020-07-07 15:08:47 +0530

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -13,15 +13,6 @@ var gulp = require('gulp'),
     inquirer = require("inquirer"),
     fs = require('fs');
 
-var deploy      = require('gulp-gh-pages');
-
-/**
- * Push build to gh-pages
- */
-gulp.task('deploy', function () {
-  return gulp.src("./visual/**/*")
-    .pipe(deploy())
-});
 gulp.task('clean', function(cb) {
     del('lib/**/*.*', cb);
 });
